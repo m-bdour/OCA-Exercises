10adf830b869f0a685bfa761f1af0984f4d453df
mhmd6bdour <mhmd6bdoor@gmail.com>
2020-10-27 21:01:21 +0300

    73.33% completed

---
+++ JavaScript/26-Oct/dist/inClassx.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Visa = function Visa(ID) {
+  var x = document.getElementById(ID);
+
+  if (x.style.visibility == "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+};
---
+++ JavaScript/26-Oct/inClassx.js
@@ -0,0 +1,9 @@
+Visa = (ID) => {
+    var x = document.getElementById(ID);
+    if (x.style.visibility == "hidden") {
+        x.style.visibility = "visible";
+
+    } else {
+        x.style.visibility = "hidden";
+    }
+}
